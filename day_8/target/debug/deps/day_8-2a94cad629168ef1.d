774bc41393094ac45285b00baacea2a2fedbb082
Reinhardt von Brandis <u20461632@tuks.co.za>
2022-12-12 15:04:35 +0200

    day 8

--- day_8/target/debug/deps/day_8-2a94cad629168ef1.d
+++
@@ -1,5 +0,0 @@
-c:\Users\user-pc\Programming\AdventOfCode_2022\day_8\target\debug\deps\day_8-2a94cad629168ef1.rmeta: src\main.rs
-
-c:\Users\user-pc\Programming\AdventOfCode_2022\day_8\target\debug\deps\day_8-2a94cad629168ef1.d: src\main.rs
-
-src\main.rs:
--- day_8/target/debug/deps/day_8-e0c74e0641dfde06.d
+++
@@ -1,5 +0,0 @@
-c:\Users\user-pc\Programming\AdventOfCode_2022\day_8\target\debug\deps\day_8-e0c74e0641dfde06.rmeta: src\main.rs
-
-c:\Users\user-pc\Programming\AdventOfCode_2022\day_8\target\debug\deps\day_8-e0c74e0641dfde06.d: src\main.rs
-
-src\main.rs:
